f99a3fcee511b91ddb830359f5a50e718c5850c3
Ordaz Romero Sergio <orcheko@gmail.com>
2023-01-15 23:34:02 -0600

    Initial Commit

---
+++ RecognitionAppSwiftUI/RecognitionAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecognitionAppSwiftUIApp.swift
+//  RecognitionAppSwiftUI
+//
+//  Created by Sergio Ordaz Romero on 15/01/23.
+//
+
+import SwiftUI
+
+@main
+struct RecognitionAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
