ad9be8055777a474892bd865793eefe51743a471
Ordaz Romero Sergio <orcheko@gmail.com>
2023-01-16 16:18:34 -0600

    Se termina el diseño de la interfáz gráfica y se hace uso del modelo para predicciones

--- RecognitionAppSwiftUI/ContentView.swift
+++ RecognitionAppSwiftUI/ContentView.swift
@@ -8,32 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var imageDescription: String = ""
+    @State private var image = UIImage()
+    @State private var isPresented = false
+    
     var body: some View {
         NavigationView {
-            VStack {
-                Image("")
-                    .resizable()
-                    .border(Color.gray.opacity(0.5), width: 0.5)
-                    .cornerRadius(35)
-                    .background(.gray)
-            }
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbarBackground(.red, for: .navigationBar)
-            .toolbarBackground(.visible, for: .navigationBar)
-            .toolbar {
-                ToolbarItem(placement: .principal) {
-                    Text("Image Analizer")
-                        .font(.title)
-                        .bold()
-                        .foregroundColor(.white)
-                }
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button {
-                        
+            VStack(spacing: 0) {
+                HStack {
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("Image Analizer")
+                            .font(.system(size: 35, weight: .semibold, design: .default))
+                        Text("Objeto analizado: " + imageDescription)
+                    }
+                    Spacer()
+                    NavigationLink {
+                        ImageAnalizer(image: $image, imageDescription: $imageDescription)
+                            .background(.black)
+                            .toolbar(.hidden)
                     } label: {
                         Image(systemName: "camera")
+                            .padding(10)
+                            .background(.opacity(0.3))
+                            .cornerRadius(5)
                     }
-                    
+                }
+                .frame(maxWidth: .infinity, alignment: .topLeading)
+                .padding()
+                .background(.red)
+                .foregroundColor(.white)
+                Divider()
+                    .frame(minHeight: 1)
+                    .background(.clear)
+                
+                //Body
+                VStack {
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .background(.gray)
                 }
             }
         }
--- RecognitionAppSwiftUI/Models/ImageAnalizer.swift
+++ RecognitionAppSwiftUI/Models/ImageAnalizer.swift
@@ -8,17 +8,76 @@
 import Foundation
 import SwiftUI
 import UIKit
+import Vision
 
-struct ImageAnalize {
+struct ImageAnalizer: UIViewControllerRepresentable {
+    @Binding var image: UIImage
+    @Binding var imageDescription: String
+    @Environment(\.presentationMode) var presentationMode
     
+    init(image: Binding<UIImage>, imageDescription: Binding<String>) {
+        self._image = image
+        self._imageDescription = imageDescription
+    }
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = context.coordinator
+        return imagePicker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(self)
+    }
     
 }
 
 class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     private var picker = UIImagePickerController()
     
-    override init() {
-        super.init()
-        picker.delegate = self
+    private var parent: ImageAnalizer
+    
+    init(_ parent: ImageAnalizer) {
+        self.parent = parent
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.editedImage] as? UIImage {
+            DispatchQueue.main.async {
+                self.parent.image = image
+            }
+            
+            if let ciimage = CIImage(image: image) {            
+                analize(with: ciimage)
+                self.parent.presentationMode.wrappedValue.dismiss()
+            }
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.parent.presentationMode.wrappedValue.dismiss()
+    }
+    
+    func analize(with image: CIImage) {
+        if let model = try? VNCoreMLModel(for: ImageRecognitionModelMaker(configuration: .init()).model) {
+            let request = VNCoreMLRequest(model: model) { request, error in
+                let firstResult = request.results!.first as! VNClassificationObservation
+                DispatchQueue.main.async {
+                    self.parent.imageDescription = firstResult.identifier
+                }
+            }
+            let handler = VNImageRequestHandler(ciImage: image)
+            do {
+                try handler.perform([request])
+            } catch {
+                print("Error analizing the photo")
+            }
+        }
     }
 }
